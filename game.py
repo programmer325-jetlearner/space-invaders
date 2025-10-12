886f7b73e60dd3eaa4f1dba385ae495f1a07de4b
programmer325-jetlearner <asyedtanim1@gmail.com>
2025-10-12 12:02:10 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -17,4 +17,16 @@ MAX_BULS=3
 SPACESHIP_WIDTH=55
 SPACESHIP_HEIGHT=40
 
+YELLOW_SPACESHIP_IMAGE=pygame.image.load(os.path.join("assets","yellow_spaceship.png"))
+YELLOW_SPACESHIP=pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),90)
+RED_SPACESHIP_IMAGE=pygame.image.load(os.path.join("assets","red_spaceship.png"))
+RED_SPACESHIP=pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),270)
+SPACE_IMG=pygame.image.load(os.path.join("assets","bg.png"))
+SPACE=pygame.transform.scale(SPACE_IMG,(900,500))
+
+def draw_window(red,yellow,red_bullets,yellow_bullets,red_health,yellow_health):
+    screen.blit(SPACE,(0,0))
+
+
+
 
