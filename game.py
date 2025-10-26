80ef00915f5ab6b2202626f2dfec97185b28d4c2
programmer325-jetlearner <asyedtanim1@gmail.com>
2025-10-26 11:58:04 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -53,6 +53,56 @@ def yellow_spaceship_movement(keys_pressed,yellow):
     if keys_pressed[pygame.K_s] and yellow.y+VEL+yellow.height<HEIGHT-15:
         yellow.y+=VEL
 
+def red_spaceship_movement(keys_pressed,red):
+    if keys_pressed[pygame.K_LEFT] and red.x-VEL>BORDER.x+BORDER.width:
+        red.x-=VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x+VEL+red.width<WIDTH:
+        red.x+VEL
+    if keys_pressed[pygame.K_UP] and red.y-VEL>0:
+        red.y-=VEL
+    if keys_pressed[pygame.K_DOWN] and red.y+VEL+red.height<HEIGHT-15:
+        red.y+=VEL
+
+
+YELLOW_HIT=pygame.USEREVENT+1
+RED_HIT=pygame.USEREVENT+2
+
+def handling_bullets(yellow_bullets,red_bullets,yellow,red):
+    for bullet in yellow_bullets:
+        bullet.x+=BUL_VEL
+        if red.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(RED_HIT))
+            yellow_bullets.remove(bullet)
+        elif bullet.x>WIDTH:
+            yellow_bullets.remove(bullet)
+    
+    for bullet in red_bullets:
+        bullet.x-=BUL_VEL
+        if yellow.colliderect(bullet):
+            pygame.event.post(pygame.event.Event(YELLOW_HIT))
+            red_bullets.remove(bullet)
+        elif bullet.x<0:
+            red_bullets.remove(bullet)
+
+def draw_winner(text):
+    draw_text=WINNER_FONT.render(text,1,"white")
+    screen.blit(draw_text,(WIDTH/2-draw_text.get_width()/2,HEIGHT/2-draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(5000)
+    
+        
+        
+
+
+    
+    
+    
+        
+
+
+
+
+
 
     
 
